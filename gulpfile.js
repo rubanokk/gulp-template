dc82959443346de99119694f9e542217db018044
rubanokk <kashilya@yandex.ru>
2015-08-25 19:18:35 +0300

    sync

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,39 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     server = lr();
 
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'html'], function() {
+
+    browserSync.init(["css/*.css", "js/*.js"], {
+        server: "template/"
+    });
+
+    gulp.watch("assets/sass/*.scss", ['sass']);
+    gulp.watch("assets/html/*.html", ['html']);
+    gulp.watch("template/*.html").on('change', browserSync.reload);
+    gulp.watch("template/css/*.css").on('change', browserSync.reload);
+});
+
+gulp.task('html', function () {
+    gulp.src('./assets/html/*.html') 
+        .pipe(rigger()) 
+        .pipe(gulp.dest('./template/')); 
+
+});
 
 gulp.task('sass', function() {
+    gulp.src('./assets/sass/style.scss')
+    .pipe(sass().on('error', console.log))
+    .pipe(autoprefixer({ browsers: ['last 2 versions'] }).on('error', console.log))
+    .pipe(gulp.dest('./template/css/'));
+});
+
+gulp.task('default', ['serve']);
+
+
+
+
+/*gulp.task('sass', function() {
     gulp.src('./assets/sass/style.scss')
     .pipe(sass().on('error', console.log))
     .pipe(autoprefixer({ browsers: ['last 2 versions'] }).on('error', console.log))
@@ -27,6 +58,8 @@ gulp.task('lr-server', function() {
     });
 })
 
+
+
 gulp.task('default', function() {  
     gulp.run('lr-server', 'sass', 'html');
 
@@ -38,3 +71,5 @@ gulp.task('default', function() {
         gulp.run('sass');
     })
 })
+
+*/
