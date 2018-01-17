712fcbd6243980629bd01aad4fe11661fc4f462c
iakashtanov <i.kashtanov@dodopizza.com>
2018-01-17 17:02:47 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -6,31 +6,27 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     server = lr();
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'html'], function() {
-
+gulp.task('watch', ['sass', 'html'], function() {
     browserSync.init(["css/*.css", "js/*.js"], {
         server: "template/"
     });
-
-    gulp.watch("assets/sass/*.scss", ['sass']);
-    gulp.watch("assets/html/*.html", ['html']);
+    gulp.watch("src/sass/*.scss", ['sass']);
+    gulp.watch("src/html/*.html", ['html']);
     gulp.watch("template/*.html").on('change', browserSync.reload);
     gulp.watch("template/css/*.css").on('change', browserSync.reload);
 });
 
 gulp.task('html', function () {
-    gulp.src('./assets/html/*.html') 
+    gulp.src('./src/html/*.html') 
         .pipe(rigger()) 
         .pipe(gulp.dest('./template/')); 
-
 });
 
 gulp.task('sass', function() {
-    gulp.src('./assets/sass/style.scss')
+    gulp.src('./src/sass/style.scss')
     .pipe(sass().on('error', console.log))
     .pipe(autoprefixer({ browsers: ['last 2 versions'] }).on('error', console.log))
     .pipe(gulp.dest('./template/css/'));
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['watch']);
